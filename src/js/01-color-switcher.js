2de833539358bd96c03ecbdc71034f0090699b01
VolodymyrBakun <119749890+VolodymyrBakun@users.noreply.github.com>
2023-05-06 20:10:04 +0300

    task1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,27 @@
+const btnStartEl = document.querySelector('[data-start]');
+const btnStopEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+
+let intervalId = null;
+
+const bgChangeInterval = function () {
+  intervalId = setInterval(() => {
+    bodyEl.style.background = `${getRandomHexColor()}`;
+  }, 1000);
+  btnStartEl.disabled = 'true';
+};
+
+const bgClearInterval = function () {
+  btnStartEl.disabled = false;
+  clearInterval(intervalId);
+};
+
+btnStartEl.addEventListener('click', bgChangeInterval);
+btnStopEl.addEventListener('click', bgClearInterval);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
